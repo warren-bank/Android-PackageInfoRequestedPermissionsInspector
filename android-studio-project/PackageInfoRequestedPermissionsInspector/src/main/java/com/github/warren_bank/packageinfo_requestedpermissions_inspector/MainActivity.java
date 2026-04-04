6ec3fdf72be5fd27ad0431b01ab069843a8898a7
Warren R. Bank <warren.r.bank@gmail.com>
2026-04-03 19:36:55 -0700

    release: '002.00.00-01API'

--- android-studio-project/PackageInfoRequestedPermissionsInspector/src/main/java/com/github/warren_bank/packageinfo_requestedpermissions_inspector/MainActivity.java
+++ android-studio-project/PackageInfoRequestedPermissionsInspector/src/main/java/com/github/warren_bank/packageinfo_requestedpermissions_inspector/MainActivity.java
@@ -1,23 +1,55 @@
 package com.github.warren_bank.packageinfo_requestedpermissions_inspector;
 
+import com.github.warren_bank.packageinfo_requestedpermissions_inspector.R;
+
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+  private static final int REQUEST_CODE_PERMISSIONS  = 0;
+
+  private TextView textview_requested_permissions;
+  private Button button_request_permissions;
+  private ArrayList<String> denied;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    TextView requested_permissions_textview = (TextView) findViewById(R.id.requested_permissions);
-    requested_permissions_textview.setText(
+    textview_requested_permissions = (TextView) findViewById(R.id.textview_requested_permissions);
+    button_request_permissions     = (Button)   findViewById(R.id.button_request_permissions);
+
+    button_request_permissions.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        if ((denied != null) && !denied.isEmpty()) {
+          requestPermissions(denied.toArray(new String[denied.size()]), REQUEST_CODE_PERMISSIONS);
+        }
+      }
+    });
+
+    reset();
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    if (requestCode != REQUEST_CODE_PERMISSIONS)
+      return;
+
+    reset();
+  }
+
+  private void reset() {
+    textview_requested_permissions.setText(
       get_requested_permissions()
     );
   }
@@ -26,46 +58,93 @@ public class MainActivity extends Activity {
     try {
         PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_PERMISSIONS);
 
-        if ((info.requestedPermissions == null) || (info.requestedPermissions.length == 0))
+        if (info.requestedPermissions == null)
           throw new Exception();
 
+        StringBuffer buf = new StringBuffer();
+
         if (Build.VERSION.SDK_INT < 16) {
-          return TextUtils.join("\n", info.requestedPermissions);
+          appendHeading(buf, getString(R.string.result_heading_all));
+          appendList(buf, info.requestedPermissions);
         }
+        else {
+          denied                    = new ArrayList<String>();
+          ArrayList<String> granted = new ArrayList<String>();
 
-        StringBuffer buf          = new StringBuffer();
-        ArrayList<String> denied  = new ArrayList<String>();
-        ArrayList<String> granted = new ArrayList<String>();
+          for (int i = 0; i < info.requestedPermissions.length; i++) {
+            if ((info.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) == 0)
+              denied.add(info.requestedPermissions[i]);
+            else
+              granted.add(info.requestedPermissions[i]);
+          }
 
-        for (int i = 0; i < info.requestedPermissions.length; i++) {
-          if ((info.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) == 0)
-            denied.add(info.requestedPermissions[i]);
-          else
-            granted.add(info.requestedPermissions[i]);
-        }
+          appendHeading(buf, getString(R.string.result_heading_denied));
+          appendList(buf, denied);
 
-        if (!granted.isEmpty()) {
-          buf.append("granted\n");
-          buf.append("=======\n");
-          buf.append(
-            TextUtils.join("\n", granted)
+          appendHeading(buf, getString(R.string.result_heading_granted));
+          appendList(buf, granted);
+
+          button_request_permissions.setVisibility(
+            (!granted.isEmpty() && (Build.VERSION.SDK_INT >= 23))
+              ? View.VISIBLE
+              : View.GONE
           );
         }
-        if (!granted.isEmpty() && !denied.isEmpty()) {
-          buf.append("\n\n");
-        }
-        if (!denied.isEmpty()) {
-          buf.append("denied\n");
-          buf.append("======\n");
-          buf.append(
-            TextUtils.join("\n", denied)
-          );
-        }
+
+        appendHeading(buf, getString(R.string.result_heading_check));
+        appendList(buf, filterByCheckCallingOrSelfPermission(info.requestedPermissions));
 
         return buf.toString();
     }
     catch (Exception e) {
         return "";
+    }
+  }
+
+  private ArrayList<String> filterByCheckCallingOrSelfPermission(String[] requestedPermissions) {
+    ArrayList<String> filtered = new ArrayList<String>();
+    for (int i = 0; i < requestedPermissions.length; i++) {
+      String permission = requestedPermissions[i];
+      if (checkCallingOrSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+        filtered.add(permission);
+      }
+    }
+    return filtered;
+  }
+
+  private void appendHeading(StringBuffer buf, String heading) {
+    String divider = new String(new char[heading.length()]).replace("\0", "=");
+
+    if (buf.length() > 0) {
+      buf.append("\n\n");
+    }
+    buf.append(heading);buf.append("\n");
+    buf.append(divider);buf.append("\n");
+  }
+
+  private void appendList(StringBuffer buf, ArrayList<String> list) {
+    if ((list == null) || list.isEmpty()) {
+      buf.append(
+        getString(R.string.result_list_empty)
+      );
+    }
+    else {
+      buf.append(
+        TextUtils.join("\n", list)
+      );
+    }
+  }
+
+  private void appendList(StringBuffer buf, String[] list) {
+    if ((list == null) || (list.length == 0)) {
+      buf.append(
+        getString(R.string.result_list_empty)
+      );
+    }
+    else {
+      buf.append(
+        TextUtils.join("\n", list)
+      );
     }
   }
 }
